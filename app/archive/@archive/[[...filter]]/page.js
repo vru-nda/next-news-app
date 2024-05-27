7fcbec86f363b1f7c17225f26e3c2141ff8dcdf0
vru-nda <vrunda1002@gmail.com>
2024-05-27 12:10:45 +0530

    catchall fallback routes

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,61 @@
+import Link from 'next/link';
+
+import NewsList from '@/components/news-list';
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from '@/lib/news';
+
+const FilteredNewsPage = ({params}) => {
+  const filters = params.filter;
+
+  const selectedYear = filters?.[0];
+  const selectedMonth = filters?.[1];
+
+  let news;
+  let links;
+
+  links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selcted year</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  return (
+    <>
+      <header id='archive-header'>
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+};
+
+export default FilteredNewsPage;
