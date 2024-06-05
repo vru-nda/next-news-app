a5730b57a57de3c3d3fd42fcae84cdec04f2c3d7
vru-nda <vrunda1002@gmail.com>
2024-06-05 16:42:06 +0530

    Close the modal and redirect

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,7 +1,10 @@
+'use client';
+
 import {DUMMY_NEWS} from '@/dummy-news';
-import {notFound} from 'next/navigation';
+import {notFound, useRouter} from 'next/navigation';
 
 const InterceptedImagePage = ({params}) => {
+  const router = useRouter();
   const newsSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((item) => item.slug === newsSlug);
 
@@ -11,7 +14,7 @@ const InterceptedImagePage = ({params}) => {
 
   return (
     <>
-      <div className='modal-backdrop' />
+      <div className='modal-backdrop' onClick={router.back} />
       <dialog className='modal' open>
         <div className='fullscreen-image'>
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
